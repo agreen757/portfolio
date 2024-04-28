376eb7516c38f9e1c707a89684cd9cfef61b7016
agreen757 <dominion.green@gmail.com>
2024-04-28 17:47:22 -0400

    content update with optimization

--- public/__settings__.js
+++ public/__settings__.js
@@ -3,12 +3,12 @@ window.SCRIPT_PREFIX = "";
 window.SCENE_PATH = "1898912.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
-    'alpha': true,
-    'preserveDrawingBuffer': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
     'deviceTypes': [`webgl2`, `webgl1`],
-    'powerPreference': "high-performance"
+    'powerPreference': "default"
 };
-window.SCRIPTS = [ 157500580, 157317742, 157838945, 158168236, 158319894, 158453141, 158584055 ];
+window.SCRIPTS = [ 157500580, 157317742, 157838945, 158168236, 158319894, 158453141, 158584055, 175892916, 176175360, 176182983 ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,4 +18,5 @@ window.INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/175903211/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/175903212/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/175903210/1/ammo.js', 'preload' : true},
 ];
